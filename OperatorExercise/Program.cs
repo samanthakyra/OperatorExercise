5c52be20cfb73ff246a21603b1ed55dde541e925
samanthakyra <samanthakyramendoza@gmail.com>
2022-09-14 07:50:31 -0700

    finished area of circle

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            double radius = 20;
+            var AreaOfCircle = Math.PI * radius * radius;
+
+            {
+                if ( a == 17 && b == 4)
+
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+                Console.WriteLine($"What is the radius of your circle?");
+                double v = double.Parse(Console.ReadLine());
+                v = radius;
+                Console.WriteLine($"The area of a circle is {AreaOfCircle}");
+
+            }
+        }
+    }
+}
+
